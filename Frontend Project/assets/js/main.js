56689d3d411a4647c2ab8b020856d9c86dd38d8c
SabinaySh <72246278+SabinaySh@users.noreply.github.com>
2021-02-06 05:37:02 -0800

    wrote testimonial section and index-categories section

--- Frontend Project/assets/js/main.js
+++ Frontend Project/assets/js/main.js
@@ -13,4 +13,40 @@ $(document).ready(function(){
         $("html,body").animate({scrollTop:0},'slow')
     });
 
+
+    $('.top-owl').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+    $('.testimonials').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
 });
