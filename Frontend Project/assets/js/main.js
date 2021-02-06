c8e042c4d8480cc4b6655735826b9e50b29d6f03
SabinaySh <72246278+SabinaySh@users.noreply.github.com>
2021-02-06 11:18:15 -0800

    wrote index page

--- Frontend Project/assets/js/main.js
+++ Frontend Project/assets/js/main.js
@@ -49,4 +49,41 @@ $(document).ready(function(){
             }
         }
     })
+
+    $('.owl-testimonial').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+    $('.owl-latest').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
 });
